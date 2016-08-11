68741af0b9b5fc9842f982cff4aaca802ccbeece
matt vickers <matt.vickers@sky.com>
2016-08-11 13:27:01 +0100

    save to CSV file function incorporated

--- directory.rb
+++ directory.rb
@@ -94,6 +94,8 @@ def interactive_menu
     when "2"
       # show the students
       show_students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else
@@ -105,6 +107,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -112,6 +115,18 @@ def show_students
   print_header
   print
   print_footer
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 #nothing happens until we call the methods
