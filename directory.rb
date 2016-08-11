e33bc5afceffc721927071470f89f85cc097e335
matt vickers <matt.vickers@sky.com>
2016-08-11 15:13:29 +0100

    added load function and moved @students outside method to fix

--- directory.rb
+++ directory.rb
@@ -1,28 +1,22 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  @students = []
   #get the first name and string alternative to .chomp
   name = gets.delete("\n")
   #while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    puts 'hobby?'
-    hobby = gets.chomp
-    puts 'nationality?'
-    nationality = gets.chomp
     puts "cohort?"
     cohort = gets.chomp
     cohort == "" ? cohort = "august" : cohort
-    cohort.to_sym
-    @students << {name: name, cohort: cohort, hobby: hobby, nationality: nationality}
+    @students << {name: name, cohort: cohort.to_sym}
     if @students.count == 1
       puts "Now we have #{@students.count} student."
     else
       puts "Now we have #{@students.count} students."
     end
-
     # get another name from the user
     name = gets.chomp
   end
@@ -35,10 +29,10 @@ def print_header
   puts "-------------".center(50)
 end
 
-def print
+def print_students
   count = 0
   while @students.length >= count+1
-    puts "#{@students[count][:name]} (#{@students[count][:cohort]} cohort) likes #{@students[count][:hobby]} nationality is #{@students[count][:nationality]}".center(50)
+    puts "#{@students[count][:name]} (#{@students[count][:cohort]} cohort)".center(50)
     count+=1
   end
 end
@@ -81,26 +75,8 @@ end
 
 def interactive_menu
   loop do
-    # 1. print the menu and ask the user what to do
     print_menu
-# 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      #input the students
-      @students = input_students
-    when "2"
-      # show the students
-      show_students
-    when "3"
-      save_students
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    process(gets.chomp)
   end
 end
 
@@ -108,19 +84,35 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def process(selection)
+  case selection
+  when "1"
+    @students = input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+  puts "I don't know what you meant, try again"
+  end
 end
 
 def show_students
   print_header
-  print
+  print_students
   print_footer
 end
 
 def save_students
-  # open the file for writing
   file = File.open("students.csv", "w")
-  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -129,7 +121,15 @@ def save_students
   file.close
 end
 
-#nothing happens until we call the methods
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 interactive_menu
 if @students.empty?
 abort("You haven't entered any students. Program will now quit")
